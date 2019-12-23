487f3b9325a122be9e3e46d19592af3ccc9f33ff
Lukasz Glib <lukasz.gl28@gmail.com>
2019-12-23 17:34:07 +0100

    add configuration package for Security and Web

---
+++ src/main/java/pl/lg/charity/config/SecurityConfiguration.java
@@ -0,0 +1,73 @@
+package pl.lg.charity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure (WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/media/**")
+                .antMatchers("/webjars/**")
+                .antMatchers("/h2-console", "/h2-console/**");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT email, password, active FROM users WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT u.email, r.name FROM users u JOIN users_roles ur ON u.id" +
+                        "= ur.user_id JOIN roles r ON ur.roles_id = r.id WHERE u.email = ?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/media/**").permitAll()
+                .antMatchers("/more/**").permitAll()
+                .antMatchers("/logout").authenticated()
+                .antMatchers("/advertisements/**").permitAll()
+                .antMatchers("/user", "/user/**").hasRole("USER")
+                .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .and()
+            .csrf();
+    }
+}
