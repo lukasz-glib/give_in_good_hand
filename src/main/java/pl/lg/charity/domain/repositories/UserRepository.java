c2f9daa06974799677e25e1f6f6f811b2c86f173
Lukasz Glib <lukasz.gl28@gmail.com>
2020-01-13 21:49:45 +0100

    add queries for creating default admin and user to heroku

--- src/main/java/pl/lg/charity/domain/repositories/UserRepository.java
+++ src/main/java/pl/lg/charity/domain/repositories/UserRepository.java
@@ -31,4 +31,28 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Transactional
     @Query(value = "UPDATE User u SET u.active = false WHERE u.id = ?1")
     void statusBlockedUser(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO users (active, email, password, last_name, username) VALUES" +
+            " (true, 'admin@gmail.com', '{noop}admin', 'admin', 'admin')", nativeQuery = true)
+    void createAdmin();
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO users_roles (user_id, roles_id) VALUES" +
+            "(SELECT id FROM users WHERE email = 'admin@gmail.com', 2)", nativeQuery = true)
+    void makeAdminAdmin();
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO users (active, email, password, last_name, username) VALUES" +
+            " (true, 'user@gmail.com', '{noop}user', 'user', 'user')", nativeQuery = true)
+    void createUser();
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO users_roles (user_id, roles_id) VALUES" +
+            "(SELECT id FROM users WHERE email = 'user@gmail.com', 1)", nativeQuery = true)
+    void makeUserUser();
 }
